afc04da2b9c89bd18f1179595fea4c6ea53320a1
Diggo Silva <rdiggosilva@gmail.com>
2025-03-10 09:50:54 -0300

    Removed some unused lines of code

---
+++ AuthenticationUserdefaultsTests/HomeViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewModelTests.swift
+//  AuthenticationUserdefaultsTests
+//
+//  Created by Diggo Silva on 09/03/25.
+//
+
+import Testing
+
+struct HomeViewModelTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
